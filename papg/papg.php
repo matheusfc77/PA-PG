b5d0566b3ae765aff88bbbec1026d39b373b785b
Matheus Candido <matheusfc77@hotmail.com>
2021-05-29 17:28:46 -0300

    Update papg.php

--- papg/papg.php
+++ papg/papg.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <?php
-    $tituloPagina = "PA PG - V1";
+    $tituloPagina = "PA PG - V2";
     $titulo = "Gerador de Progressões Aritméticas e Geométricas";
+
+    $subtitulo1 = "Gerador de Progressões";
+    $subtitulo2 = "Upload de Progressões";
 
     $a1 = 0;
     if(isset($_GET['a1'])) {
@@ -39,10 +42,22 @@
         }
     }
 
-    $dados_json = json_encode($progressao);
+    $dados_json = array(
+        "a1" => $a1,
+        "razao" => $razao,
+        "qtd" => $qtd,
+        "progressao" => $progressao,
+        "tipo" => $tipo
+    );
+    $json = json_encode($dados_json);
     $fp = fopen($file.".json", "w");
-    fwrite($fp, $dados_json);
+    fwrite($fp, $json);
     fclose($fp);
+
+    $arquivoUpload = "";
+    if(isset($_GET['arquivoUpload'])) {
+        $arquivoUpload= $_GET['arquivoUpload'].".json";
+    }
 ?>
 <html>
 <head>
@@ -51,13 +66,14 @@
 </head>
 <body>
     <h1><?php echo $titulo ?></h1>
+    <h2><?php echo $subtitulo1 ?></h2>
         
     <form action="" method="get">
-        Primeiro termo (a1) <input type="text" name="a1" id="a1" value=""></input><br>
-        Razão <input type="text" name="razao" id="razao" value=""></input><br>
-        Qtd elementos <input type="text" name="qtd" id="qtd" value=""></input><br>
-        PA ou PG? <input type="text" name="tipo" id="tipo" value=""></input><br>
-        Nome do arquivo <input type="text" name="file" id="file" value=""></input><br><br>
+        Primeiro termo (a1) <input type="text" name="a1" id="a1" value="" required></input><br>
+        Razão <input type="text" name="razao" id="razao" value="" required></input><br>
+        Qtd elementos <input type="text" name="qtd" id="qtd" value="" required></input><br>
+        PA ou PG? <input type="text" name="tipo" id="tipo" value="" required></input><br>
+        Nome do arquivo <input type="text" name="file" id="file" value="" required></input><br><br>
         <input type="submit" name="button1" id="button1" value="Gerar Progressão!"/>
     </form>
 
@@ -67,7 +83,49 @@
         }
     ?>
 
-    <h1><?php echo $a1 ?></h1>
+    <h2><?php echo $subtitulo2 ?></h2>
+
+    <form action="" method="get">
+        Nome do arquivo <input type="text" name="arquivoUpload" id="arquivoUpload" value=""></input>
+        <input type="submit" name="button2" id="button2" value="Upload!"/>
+    </form>
+    
+    <?php
+        if(file_exists($arquivoUpload)) {
+            $arquivo = file_get_contents($arquivoUpload);
+            $dados_arquivo = json_decode($arquivo);
+            echo "Elementos: ";
+            foreach($dados_arquivo->progressao as $elem) {
+                echo $elem." ";
+            }
+            
+            echo "<br>A1: ".$dados_arquivo->a1;
+            echo "<br>Razão: ".$dados_arquivo->razao;
+            echo "<br>PA ou PG? ".$dados_arquivo->tipo;
+            echo "<br>Quantidade de elementos: ".$dados_arquivo->qtd;
+
+            $soma = 0;
+            foreach($dados_arquivo->progressao as $elem) {
+                $soma += $elem;
+            }
+            
+            $media = $soma / $dados_arquivo->qtd;
+            $valoresOrdenados = sort($dados_arquivo->progressao);
+            $mediana = 0;
+            if ($dados_arquivo->qtd % 2 == 0) {
+                $mediana = ($dados_arquivo->progressao[$dados_arquivo->qtd / 2 - 1] + $dados_arquivo->progressao[$dados_arquivo->qtd / 2]) / 2;
+            } else {
+                $mediana = $dados_arquivo->progressao[intdiv($dados_arquivo->qtd, 2)];
+            }
+
+            echo "<br>Somatória: ".$soma;
+            echo "<br>Média: ".$media;
+            echo "<br>Mediana: ".$mediana;
+
+        } else {
+            echo "<br>Arquivo não encontrado";
+        }
+    ?>
 
 </body>
 </html>
